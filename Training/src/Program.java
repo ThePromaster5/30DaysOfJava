86648dde5d07139a0f9182a56980835fe24987b7
ThePromaster5 <jacobayoub1@yahoo.com>
2020-08-03 23:59:16 -0400

    initial commit

---
+++ Training/src/Program.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) { 
+		// TODO Auto-generated method stub
+		System.out.println("Enter your name"); 
+		
+		//Scanner in = new Scanner(System.in);
+		//String s = in.nextLine();
+		
+		
+		int i = 0;
+		do {
+			System.out.println("i is: " + i);
+			i++;
+		} while(1 < 10);
+	
+		Scanner in;
+		in.close();
+	}
+}
